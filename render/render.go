d234e7fb7be6f96171acf7e08425bfdda56d47cd
nireo <eemil.lehtikangas1@gmail.com>
2022-08-14 14:14:52 +0300

    yes

--- render/render.go
+++ render/render.go
@@ -1,8 +1,26 @@
 package render
 
-import "github.com/yuin/goldmark"
+import (
+	"os"
+	"path/filepath"
+)
 
 type Renderer struct {
-	md        goldmark.Markdown
+	// all of the posts are cached in memory since they don't take up that much space
+	// and they're quicker to retrieve.
+	posts     map[string][]byte
 	directory string
+}
+
+func New(dir string) (*Renderer, error) {
+	rdr := &Renderer{
+		posts:     make(map[string][]byte),
+		directory: dir,
+	}
+
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		return nil
+	})
+
+	return rdr, nil
 }
